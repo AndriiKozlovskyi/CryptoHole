3d7b8472b71fa80ed2e58a2520497c11221766a2
jstand <akiricuk37@gmail.com>
2024-06-21 02:35:14 +0200

    ! ProjectDescriptionView.vue
    * New Functionality
    -- Adding Toast component and for writing logic for notification when adding and deleting project from "Saved project"
    -- Adding new button "arrow back"  for exiting from element "Project Description" to "All Project"
    -- Adding amount of participants and how much will cost one account with link to the project.
    -- Adding hashtag to project for better searching functionality.
    -- Adding button "Saved Project" in element "Project Description" for user friendly design.
    
    * New Design
    -- Adding arrow left for exiting from project description.
    -- Remaking top position for aside element for centalizing it with element "Side Bar"
    -- Changing image to right project and style for better resolution.
    -- Changing design of "Description" title and text below title.
    -- Adding hover animation for exiting button.
    -- Adding hover animation on button "Saved Project"
    -- Adding hover background color changing animation for link to the project.
    - Adding hover animation for hashtag.

--- src/views/ProjectDescriptionView.vue
+++ src/views/ProjectDescriptionView.vue
@@ -1,4 +1,5 @@
 <template>
+  <Toast/>
   <div class="flex h-100% w-full flex-row justify-start items-center ">
     <main class="w-full h-full flex justify-between font ">
       <div class="absolute h-100% flex  flex-col mt-[1rem] space-y-10 ml-[5rem] max-w-[40rem] ">
@@ -59,32 +60,98 @@
                 </ul>
             </div>
             </div>
-
-        <div class=" top-0 bottom-0 fixed left-[7.1rem] transform translate-x-1 w-1 bg-gray-500 z-0 "></div>
+        <div class=" top-0 bottom-0 fixed left-[7.1rem] transform translate-x-1 w-1 bg-gray-500 z-0"></div>
       </div>
-
-      <aside class=" w-[32rem] h-[47rem] flex flex-col rounded-lg self-center fixed left-[55rem] top-[6.5rem]  ">
+      
+      <aside class=" w-[32rem] h-[47rem] flex flex-col rounded-lg self-center fixed left-[55rem] top-[5.5rem]">
         <div class="mt-10 ml-auto mr-auto">
-          <img class="" src="https://drift-boss.io/cache/data/image/game/drift-io-m672x378.png" />
+          <i class="pi pi-arrow-left text-gray-500 text-2xl top-[1rem] right-[1rem] absolute hover:cursor-pointer transition-transform duration-300 hover:-translate-x-2" @click="router.go(-1)"></i>
+
+        <div class="relative">
+           <img src="https://cdn.prod.website-files.com/6310e7dee49f0866da8eed4c/63adcc1db02d660497f9957e_Drift%20Full%20Logo%20White.png" alt="Logo" class="w-full h-auto" />
+           <p class="absolute top-3 left-3 text-white w-[4rem] h-[2rem] flex justify-center items-center bg-gray-500 bg-opacity-50  rounded-full hover:bg-opacity-100 cursor-pointer"><i class=" pi pi-hashtag"></i>div.v</p>
         </div>
-        <p class="text-start pl-2 mt-8 font-extrabold text-2xl text-white">Description:</p>
-        <div class="mt-5 ml-auto mr-auto rounded-lg p-5 text-gray-400 ">
+    <div class="flex flex-col space-y-5 mt-8 text-sm relative">
+      <div class="flex flex-row space-x-3">
+        <i class="pi pi-link text-gray-400 self-center text-large"></i>
+        <a class="text-blue-500 font-bold text-large hover:text-blue-400" target="_blank" href="https://www.drift.trade/drift-governance-token">Drift Governance Token</a>
+ <button class="bg-[rgb(16,17,20)] w-[5rem] h-[5rem] hover:bg-[rgb(30,31,34)] rounded-full absolute right-0 bottom-[3rem]" @click.stop="saved ? unsave() : save()">
+        <i v-if="saved" class="pi pi-bookmark-fill text-white text-xl"></i>
+        <i v-if="!saved" class="pi pi-bookmark text-white text-xl"></i>
+</button>
+
+      </div>
+        <div class='flex flex-row space-x-3 text-sm '>
+          <i class="pi pi-users text-large text-gray-400 self-center"></i>
+          <p class="text-white">1000 / 5000</p>
+        </div>
+        <div class="flex flex-row space-x-3 text-sm">
+        <i class="pi pi-wallet text-large text-gray-400 self-center"></i>
+        <p class="text-white flex items-center"><i class="pi pi-dollar text-gray-500 text-sm "></i>3000 - 5000</p>
+      </div>
+    </div>
+  </div>
+        <p class="text-start pl-2 mt-10 font-extrabold text-2xl text-white">Description:</p>
+        <div class="mt-3 ml-auto mr-auto rounded-lg p-5 text-gray-400 ">
           <p>Drift is a fully on-chain perpetual and spot DEX built on Solana. The exchange offers traders the ability to trade pre-launch markets and launched tokens with up to 10x leverage. Beyond stablecoins, traders can use a wide range of assets as collateral, allowing for greater capital efficiency.</p>
         </div>
       </aside>
     </main>
   </div>
+  
 </template>
 
 <script setup lang="ts">
-import LocalStorageManager from "@/manager/local_storage_manager";
-import {ref} from "vue";
 
-// const project = ref(LocalStorageManager.getProjectByName())
+import LocalStorageManager from '@/manager/local_storage_manager';
+import {onMounted, ref } from 'vue';
+import { useRoute } from 'vue-router';
+import ToastManager from "@/manager/toaster_manager";
+import { useToast } from "primevue/usetoast";
+import { useRouter } from 'vue-router';
+
+const router = useRouter();
+const toast = useToast();
+const route = useRoute();
+const saved = ref(false);
+const project = ref();
+
+const isHovering = ref(false)
+
+const startAnimation = () => {
+  isHovering.value = true
+}
+
+const stopAnimation = () => {
+  isHovering.value = false
+}
+
+onMounted(() => {
+   project.value = LocalStorageManager.getProjectByName(route.params.name); 
+});
+
+ const save = () => {
+    if (project.value !== undefined)
+        LocalStorageManager.saveProject(project.value)
+     saved.value = true;
+     ToastManager.showSuccessToast(toast, "You've been successfully saved a project")
+ };
+
+ const unsave = () => {
+ const savedProject = LocalStorageManager.getSavedProjectByName(project?.value.name);
+     if (project.value !== undefined)
+        LocalStorageManager.unsaveProject(savedProject)
+    saved.value = false;
+    ToastManager.showSuccessToast(toast, "You've been successfully unsaved a project")
+};
+
 </script>
 
 <style module>
+
+
 .font {
   font-family: 'Montserrat', sans-serif;
 }
+
 </style>
