436496995de2c5a4d9d4abf7dcd8281e0cd49069
jstand <akiricuk37@gmail.com>
2024-06-25 17:28:11 +0200

    Spliting 'Aside' and 'Main' components on 2 files

---
+++ src/components/project_description/TaskMain.vue
@@ -0,0 +1,24 @@
+<template>
+  <section class="flex items-stretch text-secondary-text-color">
+    <div class="bg-[#1e1f24] text-white font-extrabold h-20 min-w-20 text-3xl flex items-center justify-center rounded-full z-10">
+      {{ numberOfTask }}
+    </div>
+    <div class="flex flex-col ml-3">
+      <h2 class="font-bold text-xl mb-2 mt-2 text-white">{{ taskTitle }}</h2>
+      <ul class="list-decimal ml-10 flex flex-col space-y-2">
+        <li v-for="(item, index) in taskDescriptionArray" :key="index">{{ item.trim() }}</li>
+      </ul>
+    </div>
+  </section>
+</template>
+
+<script setup lang="ts">
+import { computed } from 'vue';
+const props = defineProps({
+  numberOfTask: Number,
+  taskTitle: String,
+  taskDescription: String,
+});
+
+const taskDescriptionArray = computed(() => props.taskDescription.split(','));
+</script>
