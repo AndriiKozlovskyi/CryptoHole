6f93688525bab11d4d9a8f2fdff7783922d42395
jstand <akiricuk37@gmail.com>
2024-07-15 12:20:15 +0200

    !SideMenuAdmin.vue -adding splitted component from user "SideMenuAdmin" and text for better understanding about where admin is. -fixing path for admin event management.
    !AdminLayout.vue
    - fixing Admin components
    
    !AdminView.vue
    - fixing Admin components
    
    !TopViewAdmin.vue
    - fixing routeName

--- src/views/AdminView.vue
+++ src/views/AdminView.vue
@@ -2,14 +2,14 @@
     <div class="h-full flex flex-col">
       <div class="flex flex-col items-center">
         <div class="flex flex-col w-full items-center space-y-6">
-          <EventsContainer />
+          <AdminEventsContainer />
           <HelpButton />
         </div>
       </div>
     </div>
   </template>
   <script setup lang="ts">
-  import EventsContainer from '@/components/event_components/EventsContainer.vue'
+  import AdminEventsContainer from '@/components/admin_components/AdminEventsContainer.vue';
   import HelpButton from '@/components/HelpButton.vue'
   </script>
   <style>
