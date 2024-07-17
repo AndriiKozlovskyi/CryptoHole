4d6aa8f54a0f2c50cc61734eba023e3dd139842f
jstand <akiricuk37@gmail.com>
2024-07-17 15:21:01 +0200

    fixing overflow for calendar, refactoring on calendar

--- src/components/admin_components/EventAdmin.vue
+++ src/components/admin_components/EventAdmin.vue
@@ -29,7 +29,7 @@
         <p class="text-white text-[17px] apple-font" v-if="!isEditing">
           {{ event?.name }}
         </p>
-        <MyInput v-if="isEditing" class="w-[13.5rem] h-[2rem] text-center" v-model="event.name" @keyup.enter="save" type="text" placeholder="Name of the event, ex: 'Drift'"/>
+        <MyInput v-if="isEditing" class="w-[13.5rem] h-[2rem]" v-model="event.name" @keyup.enter="save" type="text" placeholder="Name of the event, ex: 'Drift'"/>
         <div class="h-full flex items-center">
           <i class="pi pi-share-alt text-secondary-text-color" />
         </div>
@@ -37,8 +37,8 @@
       <hr class="w-full border-secondary-text-color opacity-25" />
       <div class="flex flex-row justify-between w-full mb-3 rounded-full">
         <ParticipantsForm :participants="event?.participants.length" />
-        <div class="flex items-center justify-center">
-          <VueDatePicker v-model="calendarDates" range v-if="!isEditing" :dark="true" :class="calendar_theme" class="z-[1006]"/>
+        <div class="w-[15rem]">
+          <VueDatePicker v-model="dates" range v-if="!isEditing" :dark="true" :teleport="true"/>
         </div>
       </div>
     </div>
@@ -57,7 +57,7 @@ import ToastManager from '@/manager/toaster_manager'
 import { useToast } from 'primevue/usetoast'
 import MyInput from '../basic_components/input/MyInput.vue'
 import VueDatePicker from '@vuepic/vue-datepicker';
-
+const dates=ref()
 
 const router = useRouter()
 const isEditing = ref(false);
@@ -91,18 +91,18 @@ const goToAdminEventDescription = () => {
   router.push({ name: 'admin_event_description', params: { id: props.event.id } })
 }
 
-const calendarDates = ref(new Date())
+// const calendarDates = ref(new Date())
 
-onMounted(() => {
-  if (props.event?.startDate) {
-    calendarDates.value = new Date(props.event.startDate)
-  }
+// onMounted(() => {
+//   if (props.event?.startDate) {
+//     calendarDates.value = new Date(props.event.startDate)
+//   }
 
-  emitter.on("changeSelectedDate", (date: Date) => {
-    calendarDates.value = date
-    selectedDayNumber.value = date.getDate();
-  })
-})
+//   emitter.on("changeSelectedDate", (date: Date) => {
+//     calendarDates.value = date
+//     selectedDayNumber.value = date.getDate();
+//   })
+// })
 
 
 </script>
@@ -142,42 +142,4 @@ onMounted(() => {
   mask-image: linear-gradient(180deg, #000, transparent 99%);
 }
 
-.calendar_theme {
-    --dp-background-color: #212121;
-    --dp-text-color: #fff;
-    --dp-hover-color: #484848;
-    --dp-hover-text-color: #fff;
-    --dp-hover-icon-color: #959595;
-    --dp-primary-color: #005cb2;
-    --dp-primary-disabled-color: #61a8ea;
-    --dp-primary-text-color: #fff;
-    --dp-secondary-color: #a9a9a9;
-    --dp-border-color: #2d2d2d;
-    --dp-menu-border-color: #2d2d2d;
-    --dp-border-color-hover: #aaaeb7;
-    --dp-border-color-focus: #aaaeb7;
-    --dp-disabled-color: #737373;
-    --dp-disabled-color-text: #d0d0d0;
-    --dp-scroll-bar-background: #212121;
-    --dp-scroll-bar-color: #484848;
-    --dp-success-color: #00701a;
-    --dp-success-color-disabled: #428f59;
-    --dp-icon-color: #959595;
-    --dp-danger-color: #e53935;
-    --dp-marker-color: #e53935;
-    --dp-tooltip-color: #3e3e3e;
-    --dp-highlight-color: rgb(0 92 178 / 20%);
-    --dp-range-between-dates-background-color: var(--dp-hover-color, #484848);
-    --dp-range-between-dates-text-color: var(--dp-hover-text-color, #fff);
-    --dp-range-between-border-color: var(--dp-hover-color, #fff);
-}
-
-.dp__main {
-  position: absolute;
-  z-index: 9999;
-}
-
-.dp__outer_menu_wrap {
-  position: fixed;
-}
 </style>
