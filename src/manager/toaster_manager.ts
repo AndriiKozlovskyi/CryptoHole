3ba195d78f650ae4480d91f6008247239a53e6c1
jstand <akiricuk37@gmail.com>
2024-07-20 00:35:48 +0200

    fix names for toast notification

--- src/manager/toaster_manager.ts
+++ src/manager/toaster_manager.ts
@@ -2,14 +2,14 @@ import type { ToastServiceMethods } from 'primevue/toastservice'
 
 export default class ToastManager {
   static showSuccessToast(toast: ToastServiceMethods, message: string) {
-    toast.add({ severity: 'success', summary: 'success', detail: message, life: 3000 })
+    toast.add({ severity: 'success', summary: 'Success', detail: message, life: 3000 })
   }
 
   static showErrorToast(toast: ToastServiceMethods, message: string) {
-    toast.add({ severity: 'error', summary: 'error', detail: message, life: 3000 })
+    toast.add({ severity: 'error', summary: 'Error', detail: message, life: 3000 })
   }
 
   static showInfoToast(toast: ToastServiceMethods, message: string) {
-    toast.add({ severity: 'info', summary: 'info', detail: message, life: 3000 })
+    toast.add({ severity: 'info', summary: 'Info', detail: message, life: 3000 })
   }
 }
